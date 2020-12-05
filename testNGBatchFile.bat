6570ba490379892a53b1b7d48b971bc45a9c8b14
Ryan kenny <ryankenny@ryans-mbp.home>
2020-12-05 10:08:29 +0000

    removed bat file

--- testNGBatchFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=/Users/ryankenny/Documents/AutomationFrameworks/DockerProject
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
